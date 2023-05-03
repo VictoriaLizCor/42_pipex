76530409fa069d4ef7292ee4843c913ba8d58b21
lilizarr <lilizarr@student.42wolfsburg.de>
2023-05-03 09:58:01 +0200

    change gitignore

--- t_pipex/src/pipex.c
+++ t_pipex/src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lilizarr <lilizarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 11:23:56 by lilizarr          #+#    #+#             */
-/*   Updated: 2023/05/02 17:39:38 by lilizarr         ###   ########.fr       */
+/*   Updated: 2023/05/03 09:56:42 by lilizarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int argc, char **argv, char **env)
 	pid_t	pid;
 	int		fd[2], nbytes;
 	char	string[] = "Hello, world!\n";
-	char	readbuffer[80];
+	char	readbuffer[20];
 
 	if (argc != 5)
 		ft_error("Invalid input");
@@ -42,27 +42,27 @@ int	main(int argc, char **argv, char **env)
 
 		/* Read in a string from the pipe */
 		nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
-		printf("Received string: %s", readbuffer);
-	}
-	ft_printf("Input : %d\n", argc);
-	while (*argv)
-	{
-		ft_printf("%s\n", *argv);
-		if (ft_strchr(*argv, ' '))
+		printf("Received string: %s\n", readbuffer);
+		ft_printf("Input : %d\n", argc);
+		while (*argv)
 		{
-			str = ft_split(*argv, ' ');
-			cpy = str;
-			while (*cpy)
+			ft_printf("%s\n", *argv);
+			if (ft_strchr(*argv, ' '))
 			{
-				ft_printf("\t\t %s\n", *cpy);
-				cpy++;
+				str = ft_split(*argv, ' ');
+				cpy = str;
+				while (*cpy)
+				{
+					ft_printf("\t\t %s\n", *cpy);
+					cpy++;
+				}
+				ft_free(str);
 			}
-			ft_free(str);
+			argv++;
 		}
-		argv++;
+		while (*env && !ft_strnstr(*env, "PATH=", ft_strlen(*env)))
+			env++;
+		ft_printf("\t\t %s\n", *env);
 	}
-	while (*env && !ft_strnstr(*env, "PATH=", ft_strlen(*env)))
-		env++;
-	ft_printf("\t\t %s\n", *env);
 	return (0);
 }
