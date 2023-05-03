07557f44d184e2a88dc2c2809dc14bf230c44b14
lilizarr <lilizarr@student.42wolfsburg.de>
2023-05-03 16:42:06 +0200

    working with fd and pipes

--- t_pipex/src/pipex.c
+++ t_pipex/src/pipex.c
@@ -6,63 +6,113 @@
 /*   By: lilizarr <lilizarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 11:23:56 by lilizarr          #+#    #+#             */
-/*   Updated: 2023/05/03 09:56:42 by lilizarr         ###   ########.fr       */
+/*   Updated: 2023/05/03 16:02:56 by lilizarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv, char **env)
+/*
+checked (R_OK for read permission, W_OK for write permission, 
+ and X_OK for execute/search permission), or the existence test (F_OK).
+*/
+void	parent_process(int *fd, char **argv, char **env)
 {
 	char	**str;
 	char	**cpy;
-	pid_t	pid;
-	int		fd[2], nbytes;
-	char	string[] = "Hello, world!\n";
-	char	readbuffer[20];
 
+	dup2(fd[1], 1);
+	close(fd[0]);
+	ft_printf("\n");
+	while (*argv)
+	{
+		ft_printf("%s\n", *argv);
+		if (ft_strchr(*argv, ' '))
+		{
+			str = ft_split(*argv, ' ');
+			cpy = str;
+			while (*cpy)
+			{
+				ft_printf("\t\t %s\n", *cpy);
+				cpy++;
+			}
+			ft_free(str);
+		}
+		argv++;
+	}
+	while (*env && !ft_strnstr(*env, "PATH=", ft_strlen(*env)))
+		env++;
+	ft_printf("\t\t %s\n", *env);
+}
+
+//int execve(const char *path, char *const argv[], char *const envp[]);
+void	child_process(int *fd, char **argv, char **env)
+{
+	int		pid;
+	char	**str;
+	char	**cpy;
+
+	pid = fork();
+	if (pid < 0)
+		ft_error("Fork failed");
+	if (!pid)
+	{
+		dup2(fd[0], 0);
+		close(fd[1]);
+		if (ft_strchr(argv[2], ' '))
+		{
+			str = ft_split(argv[2], ' ');
+			cpy = str;
+			while (*cpy)
+			{
+				ft_printf("\t\t  %s\n", *cpy);
+				cpy++;
+			}
+		}
+		else
+			str = argv + 2;
+		if (execve(str[0], &argv[2], env) < 0)
+			perror(str[0]);
+		ft_printf("\t\t %s \n", str[0]);
+		if (str != argv + 2)
+			ft_free(str);
+	}
+	ft_printf("CHILD PROCESS\n\n\n");
+}
+
+int	main(int argc, char **argv, char **env)
+{	
+	pid_t		pid;
+	int		status;
+	int		fd[2];
+
+	ft_printf("Input : %d\n", argc);
 	if (argc != 5)
 		ft_error("Invalid input");
+	while (*env && !ft_strnstr(*env, "PATH=", ft_strlen(*env)))
+		env++;
+	ft_printf("\t\t %s\n", *env);
 	if (pipe(fd) < 0)
 		ft_error("Failed to create pipe");
 	pid = fork();
-	if (pid == -1)
+	if (pid < 0)
 		ft_error("Fork failed");
-	if(pid == 0)
-	{
-		/* Child process closes up input side of pipe */
-		close(fd[0]);
-		/* Send "string" through the output side of pipe */
-		write(fd[1], string, (ft_strlen(string) + 1));
-	}
+	if (!pid)
+		child_process(fd, argv, env);
 	else
 	{
-		/* Parent process closes up output side of pipe */
-		close(fd[1]);
-
-		/* Read in a string from the pipe */
-		nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
-		printf("Received string: %s\n", readbuffer);
-		ft_printf("Input : %d\n", argc);
-		while (*argv)
+		while ((pid = wait(&status) != -1))
 		{
-			ft_printf("%s\n", *argv);
-			if (ft_strchr(*argv, ' '))
+			ft_printf("%d\n", errno);
+			fprintf(stderr, "\n\nprocess %d exits with %d\n", pid, WEXITSTATUS(status));
+			if (pid)
 			{
-				str = ft_split(*argv, ' ');
-				cpy = str;
-				while (*cpy)
-				{
-					ft_printf("\t\t %s\n", *cpy);
-					cpy++;
-				}
-				ft_free(str);
+				parent_process(fd, argv, env);
+				break;
 			}
-			argv++;
+			if ((errno != EINTR))
+				perror("waitpid");
 		}
-		while (*env && !ft_strnstr(*env, "PATH=", ft_strlen(*env)))
-			env++;
-		ft_printf("\t\t %s\n", *env);
 	}
 	return (0);
 }
--- t_pipex/test/test.c
+++ t_pipex/test/test.c
@@ -6,7 +6,7 @@
 /*   By: lilizarr <lilizarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 17:19:28 by lilizarr          #+#    #+#             */
-/*   Updated: 2023/05/03 09:44:15 by lilizarr         ###   ########.fr       */
+/*   Updated: 2023/05/03 13:38:56 by lilizarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 #include <limits.h>
 #include <stdlib.h>
 
-void	runpipe(int pfd[], char **cmd1, char **cmd2)
+void	runpipe(int pfd[], char **cmd1, char **cmd2, char **env)
 {
 	int	pid;
 
@@ -29,34 +29,34 @@ void	runpipe(int pfd[], char **cmd1, char **cmd2)
 	case 0: /* child */
 		dup2(pfd[0], 0);
 		close(pfd[1]);	/* the child does not need this end of the pipe */
-		execvp(cmd2[0], cmd2);
+		execve(cmd2[0], cmd2, env);
 		perror(cmd2[0]);
-
 	default: /* parent */
 		dup2(pfd[1], 1);
 		close(pfd[0]);	/* the parent does not need this end of the pipe */
 		execvp(cmd1[0], cmd1);
 		perror(cmd1[0]);
-
 	case -1:
 		perror("fork");
 		exit(1);
 	}
 }
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **env)
 {
 	int	pid, status;
 	int	fd[2];
-	char *cmd1[] = { "/bin/ls", "-al", "/", 0 };
+	char *cmd1[] = { "/bin/cat", "./infile", 0 };
 	char *cmd2[] = { "/usr/bin/tr", "a-z", "A-Z", 0 };
 	pipe(fd);
 
 	switch (pid = fork()) {
 		case 0: /* child */
-			runpipe(fd, cmd1, cmd2);
+			runpipe(fd, cmd1, cmd2, env);
 			exit(0);
 		default: /* parent */
+			pid = wait(&status);
+			printf("pid %d | status %d\n", pid, status);
 			while ((pid = wait(&status)) != -1)
 				fprintf(stderr, "\n\nprocess %d exits with %d\n", pid, WEXITSTATUS(status));
 			break;
